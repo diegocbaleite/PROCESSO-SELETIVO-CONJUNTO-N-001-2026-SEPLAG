0e81edeaca8a9d29750cb321d3f921964daecef3
Diego Assunção <dev.diegocba@gmail.com>
2026-01-27 19:13:49 -0400

    feat: adiciona configuração de segurança inicial - Criação da classe SecurityConfig - CSRF desabilitado - Todas as requisições liberadas (permitAll)

---
+++ src/main/java/br/gov/mt/artistas_api/ArtistasApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.artistas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/mt/artistas_api/ArtistasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mt.artistas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
