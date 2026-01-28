cd518b94e713acd9a81b0f1d564d63a131f47b43
Diego Assunção <dev.diegocba@gmail.com>
2026-01-27 20:32:25 -0400

    feat: Implantação de migração Flyway para inserir dados iniciais de artistas e álbuns

---
+++ src/main/resources/db/migration/V4__insert_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO artista (nome, tipo)
+VALUES ('Serj Tankian', 'CANTOR'),
+       ('Guns N'' Roses', 'BANDA');
+
+INSERT INTO album (nome)
+VALUES ('Harakiri'),
+       ('Use Your Illusion I');
+
+INSERT INTO artista_album (artista_id, album_id)
+VALUES (1, 1),
+       (2, 2);
+
